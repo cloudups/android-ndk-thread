9df00ff4d9564bbe81389124b36201f99122f1cd
shifeiqi <shifeiqi@kunpo.cc>
2016-12-23 13:01:16 +0800

    增加注释

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -15,21 +15,28 @@ Java_com_cloudups_thread_MainActivity_createThread(JNIEnv *env, jobject instance
     int error;
     void *ret1;
     void *ret2;
+
+    //创建第一个线程
     pthread_t pthread1;
     error = pthread_create(&pthread1, NULL, thread_fun1, (void *) "this is a new thread: ");
 
+    //判断线程创建成功与否
     if (error == 0) {
         __android_log_print(ANDROID_LOG_INFO, "jni", "create thread 1 success");
+        //等待子线程返回
         pthread_join(pthread1, &ret1);//调用此函数，主线程会阻塞在这个地方直到子线程返回
     }
     else
         __android_log_print(ANDROID_LOG_INFO, "jni", "create thread 1 fail");
 
 
+    //创建第二个线程
     pthread_t pthread2;
     error = pthread_create(&pthread2, NULL, thread_fun2, (void *) "this is a new thread: ");
 
+    //判断第二个线程创建成功与否
     if (error == 0) {
+        //等待子线程返回
         pthread_join(pthread2, &ret2);
         __android_log_print(ANDROID_LOG_INFO, "jni", "create thread 2 success");
     }
@@ -50,9 +57,9 @@ void *thread_fun1(void *message) {
     pid = getppid();
     tid = gettid();
 
-    sleep(7);
     __android_log_print(ANDROID_LOG_INFO, "jni", "%s,pid = %d,tid = %d", message, pid, tid);
 
+    //子线程正常结束
     return (void *) 1;
 }
 
@@ -66,5 +73,6 @@ void *thread_fun2(void *message) {
 
     __android_log_print(ANDROID_LOG_INFO, "jni", "%s,pid = %d,tid = %d", message, pid, tid);
 
+    //调用中介子线程方法，子线程非正常结束
     pthread_exit((void *) 2);
 }
